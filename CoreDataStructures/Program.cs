74233f67df3b6c45bdcb3dcb348fd36bc131e6c1
bmccoy04 <bryanmccoy04@gmail.com>
2017-05-12 20:08:42 -0500

    Initial commit

---
+++ CoreDataStructures/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CoreDataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Node
+    {
+        public Node Next { get; set; }
+
+        public Object Data { get; set; }
+    }
+
+    public class LinkedList
+    {
+        Node _head;
+
+        public void AddFront(Object o){
+            var node = new Node
+            {
+                Data = o,
+                Next = _head
+            };
+
+            _head = node;
+        }
+    }
+}
