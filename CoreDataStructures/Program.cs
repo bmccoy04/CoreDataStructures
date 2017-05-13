cf89e6b626c08f2744554a98251c6a41f952d1bf
bmccoy04 <bryanmccoy04@gmail.com>
2017-05-12 20:55:56 -0500

    Added list functionality.

--- CoreDataStructures/Program.cs
+++ CoreDataStructures/Program.cs
@@ -7,28 +7,85 @@ namespace CoreDataStructures
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var list = new LinkedList<Person>();
+            list.Add(new Person {Id = 1, Name = "NAme"});
+            list.Add(new Person {Id = 2, Name = "Another"});
+            var p = list.Where(x => x.Id == 1);
+            Console.WriteLine(p.Name);
+            var pTwo = list.Where(x => x.Id == 2);
+            Console.Write(pTwo.Name);
+
+            var pNull = list.Where(x => x.Id == 4);
+
         }
     }
 
-    public class Node
+    public class Node<T>
     {
-        public Node Next { get; set; }
+        public Node<T> Next { get; set; }
 
-        public Object Data { get; set; }
+        public T Data { get; set; }
     }
 
-    public class LinkedList
+    public class LinkedList<T>
     {
-        Node _head;
+        Node<T> _head;
 
-        public void AddFront(Object o){
-            var node = new Node
+        public void Add(T o){
+            var node = new Node<T>
             {
                 Data = o,
                 Next = _head
             };
 
-            _head = node;
+            if (_head == null){
+                _head = node;
+                return;
+            }
+                
+
+            var current = _head;
+            do
+            {
+                if (current.Next != null)
+                    current = current.Next;
+                
+            } while (current.Next != null);
+
+            current.Next = node;
+        }
+
+        public T GetLast(){
+            return _head.Data;
+        }
+
+        public T Where(Func<T, bool> f)
+        {
+            var current = _head;
+            do
+            {
+                if (f(current.Data))
+                    return current.Data;
+
+                current = current.Next;
+            } while (current.Next != null);
+
+            return (T)new object();
+        }
+    }
+
+    public class Person 
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+        public String Name
+        {
+            get;
+            set;
         }
     }
 }
